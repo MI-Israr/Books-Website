759d64fd5c1b000946b2b6c2cefff195053d3118
MI-Israr <124520126+MI-Israr@users.noreply.github.com>
2023-09-22 11:51:13 +0500

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,62 @@
+gasp.to("#page-1",{
+    scrollTrigger:{
+        trigger: `#page-1`,
+        start: `top top`,
+        end:`bottom top`,
+        markers:true,
+        pin:true,
+        scroller: `#main`
+    }
+})
+
+gasp.to("#page-2",{
+    scrollTrigger:{
+        trigger: `#page-2`,
+        start: `top top`,
+        end:`bottom top`,
+        markers:true,
+        pin:true,
+        scroller: `#main`
+    }
+})
+
+gasp.to("#page-3",{
+    scrollTrigger:{
+        trigger: `#page-3`,
+        start: `top top`,
+        end:`bottom top`,
+        markers:true,
+        pin:true,
+        scroller: `#main`
+    }
+})
+gasp.to("#page-4",{
+    scrollTrigger:{
+        trigger: `#page-4`,
+        start: `top top`,
+        end:`bottom top`,
+        markers:true,
+        pin:true,
+        scroller: `#main`
+    }
+})
+gasp.to("#page-5",{
+    scrollTrigger:{
+        trigger: `#page-5`,
+        start: `top top`,
+        end:`bottom top`,
+        markers:true,
+        pin:true,
+        scroller: `#main`
+    }
+})
+gasp.to("#page-6",{
+    scrollTrigger:{
+        trigger: `#page-6`,
+        start: `top top`,
+        end:`bottom top`,
+        markers:true,
+        pin:true,
+        scroller: `#main`
+    }
+})
